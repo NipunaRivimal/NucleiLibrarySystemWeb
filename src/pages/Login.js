caeb434cd8154425ac2a9cd6eaad3cda55b3fc5d
nipuna rivimal <nipuna.rivimal@gmail.com>
2020-09-28 08:44:37 +0530

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="page-content">
+      <div>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Email</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Password</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <Button variant="outline-success" style={{ width: "100%" }}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
