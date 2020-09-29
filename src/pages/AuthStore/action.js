66dab7cf8854a44252346568e5488aa19433fa56
nipuna rivimal <nipuna.rivimal@gmail.com>
2020-09-29 15:28:29 +0530

    save jwt token in local

--- src/pages/AuthStore/action.js
+++ src/pages/AuthStore/action.js
@@ -8,6 +8,13 @@ export const loginAction = (userCredintials) => {
       .then((respose) => {
         if (respose.data.status === 200) {
           dispatch({ type: "LOGIN_SUCCESS", payload: respose.data.data });
+          localStorage.setItem(
+            "login",
+            JSON.stringify({
+              login: true,
+              token: respose.data.token,
+            })
+          );
         } else if (respose.data.status === 600 || respose.data.status === 601) {
           dispatch({ type: "LOGIN_FAIL", payload: respose.data.message });
         } else {
