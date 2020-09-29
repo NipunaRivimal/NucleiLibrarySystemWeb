5ea28c4285a7f0c4dd838e44db3d8b6b1fe4bba2
nipuna rivimal <nipuna.rivimal@gmail.com>
2020-09-29 11:18:52 +0530

    increase email and password validation

--- src/customFunctions/validateAddMember.js
+++ src/customFunctions/validateAddMember.js
@@ -18,9 +18,13 @@ export default function validateAddMember(values) {
   }
   if (!values.email) {
     errors.email = "Email address is required!";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Please enter a valida email!";
   }
   if (!values.password) {
     errors.password = "Password is required!";
+  } else if (values.password.length < 6) {
+    errors.password = "Password needs to be more than 6 characters!";
   }
   return errors;
 }
--- src/customFunctions/validateLogin.js
+++ src/customFunctions/validateLogin.js
@@ -3,9 +3,13 @@ export default function validateAddBook(values) {
 
   if (!values.email) {
     errors.email = "Email is required!";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Please enter a valida email!";
   }
   if (!values.password) {
     errors.password = "Password is required!";
+  } else if (values.password.length < 6) {
+    errors.password = "Password needs to be more than 6 characters!";
   }
   return errors;
 }
